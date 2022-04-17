c9c5c1a26c8e13bd2fd0bb9a0134d9353276b782
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-17 19:15:00 +0800

    Delete initdb.sh

--- initdb.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-SEEDS_DIR="./seeds"
-for seed in $SEEDS_DIR/*; do
-    docker cp $seed trabalho_bd_database_1:seeds
-    docker exec trabalho_bd_database_1 psql -U postgres -h localhost -p 5432 -d trabalho_bd_sql -f $seed
-done;
